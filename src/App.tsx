f45bcdbc683ec72bb67838377497e49abdb30244
Paseru <hyrakalache@gmail.com>
2025-11-21 06:04:31 +0100

    Fix duplicate VoiceChatManager component causing connection conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -92,7 +92,6 @@ export default function App() {
                 <>
                     <Chat />
                     <VoiceChatManager />
-                    <VoiceChatManager />
                     <VoiceIndicator />
                     <MobileControls />
 
